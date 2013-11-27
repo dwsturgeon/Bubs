573b3dd683eb24543511b265b965314a6fffac17
Devin Sturgeon <dwsturgeon@mediafu.se>
2013-11-26 19:46:43 -0500

    Initial Commit

---
+++ Bubs/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Bubs
+//
+//  Created by Devin Sturgeon on 11/26/13.
+//  Copyright (c) 2013 Devin Sturgeon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
